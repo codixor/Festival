59e6f49a9e743d3ad53bda75ae743c01b4dd9f9d
codixor <mido.ghazali@gmail.com>
2015-11-19 00:06:31 +0100

    Update DatabaseProvider.php

--- src/Providers/DatabaseProvider.php
+++ src/Providers/DatabaseProvider.php
@@ -2,6 +2,8 @@
 namespace Wandu\Festival\Providers;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
 use Wandu\DI\ContainerInterface;
 use Wandu\DI\ServiceProviderInterface;
 
@@ -9,16 +11,20 @@ class DatabaseProvider implements ServiceProviderInterface
 {
     public function boot(ContainerInterface $app)
     {
+		$app->get(Manager::class)->setAsGlobal();
+            
+		$app->get(Manager::class)->bootEloquent();
     }
 
     public function register(ContainerInterface $app)
     {
-        $app->closure(Capsule::class, function (ContainerInterface $app) {
-            $capsule = new Capsule();
-            $default = $app->config('database.default');
-            $capsule->addConnection($app->config("database.connections.{$default}"));
-            $capsule->setAsGlobal();
-            $capsule->bootEloquent();
+        $app->closure(Manager::class, function (ContainerInterface $app) {
+            $capsule = new Capsule;
+           
+			$default = $app->config('database.default');
+            $capsule->addConnection($app->config("database.connections.{$default}"));			
+			$capsule->setEventDispatcher(new Dispatcher(new Container));
+			
             return $capsule;
         });
     }
